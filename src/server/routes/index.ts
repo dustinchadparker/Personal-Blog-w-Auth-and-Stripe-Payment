4af8e5e1c5b9167047717e23ef2a25dfae92e671
Dustin Chad Parker <dustin.chad.parker@gmail.com>
2019-03-13 09:16:21 -0500

    :

--- src/server/routes/index.ts
+++ src/server/routes/index.ts
@@ -2,10 +2,12 @@ import * as express from "express";
 
 import authRouter from "./auth";
 import apiRouter from "./api";
+import paymentRouter from "./payment";
 
 const router = express.Router();
 
 router.use("/auth", authRouter);
 router.use("/api", apiRouter);
+router.use("/payment", paymentRouter)
 
 export default router;
